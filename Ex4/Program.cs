30c89a39b23a30255a7a6321d5ba221e79fa8ae3
«Nikolay <antonooff_2nic@mail.ru>
2023-01-06 21:49:38 +0800

    добавлена блок-схема и код задачи 8

---
+++ Ex4/Program.cs
@@ -0,0 +1,18 @@
+﻿int N;
+Console.WriteLine("введите число: ");
+N = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[N];
+
+int index = 1;
+while (index < N)
+{
+    if (index % 2 == 0)
+    {
+        Console.WriteLine(index);
+        index = index + 1;
+    }
+    index = index + 1;
+}
+
+
+
